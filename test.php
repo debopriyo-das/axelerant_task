1d618c19d45890d4a1ec836765bf89235b01f426
gunjit_tti <gunjit93@gmail.com>
2017-07-14 16:23:45 +0530

    new file added test.php by Gunjit

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+	/*$a = 5;
+	$b = 6;
+	print_r($a + $b);*/
+	
+	$arr = array(0=> array("id"=>1,
+						   "name"=>"gunjit",
+						   "email"=>"abcd@gmail.com",
+						   "phone"=>1234),
+				 1=> array("id"=>2,
+						  "name"=>"qwerty",
+						  "email"=>"qwerty@gmail.com",
+						  "phone"=>789)
+			    );
+	print_r($arr[0]['id'].$arr[0]['name'].$arr[0]['email'].$arr[0]['phone']  );
+ ?>
+ 
+ 
